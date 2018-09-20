baf314973d4bb82014a962bb95731e0da65e0d3b
Luana Meira <luanameiracfc@gmail.com>
2018-09-20 19:41:03 -0300

    Validando e adicionando botões

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// let senha = document.querySelector(".senha").value;
 
-
-let botaoEnviar = document.querySelector("#btn");
+let botaoEnviar = document.querySelector("#botaoVL");
 
 botaoEnviar.addEventListener ("click", function(){
          let usuario = document.querySelector("#usuario").value;
-         if (usuario === "") { 
-        alert ("O campo login deve ser preenchido");
+         let senha = document.querySelector(".senha").value;
+         if (usuario.trim() === "" || senha.length < 7) { 
+        alert ("Os campos não foram preenchidos corretamente");
         } else  {
         alert ("Login realizado com sucesso");
      }})
