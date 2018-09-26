acff879e181433c6d6d9e3051969bf8a181df36f
Luana Meira <luanameiracfc@gmail.com>
2018-09-26 00:51:04 -0300

    Funcionamento do botão genero e opção

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 
-let botaoEnviar = document.querySelector("#botaoVL");
+let botaoVL = document.querySelector("#botaoVL");
 
-botaoEnviar.addEventListener ("click", function(){
+botaoVL.addEventListener ("click", function(event){
+    event.preventDefault()
+
          let usuario = document.querySelector("#usuario").value;
          let senha = document.querySelector(".senha").value;
          if (usuario.trim() === "" || senha.length < 7) { 
         alert ("Os campos não foram preenchidos corretamente");
         } else  {
-        alert ("Login realizado com sucesso");
+        alert ("Login válido");
      }})
 
+let botaoVG = document.querySelector("#botaoVG");
+botaoVG.addEventListener ("click", function(event){
+    event.preventDefault()
 
+    let feminino = document.getElementById("feminino");
+    let masculino = document.getElementById("masculino");
+    let outro = document.getElementById("outro");
+
+    if (feminino.checked == true || masculino.checked == true || outro.checked == true){
+    alert ("Gênero selecionado");
+    } else {
+        alert ("Nenhum gênero foi selecionado")
+
+    }
+    })
+
+    let botaoVO = document.getElementById("botaoVO");
+    botaoVO.addEventListener("click", function(event){
+        event.preventDefault()
+
+        let estagio = document.getElementById("estagio");
+        let clt = document.getElementById("clt");
+        let autonomo = document.getElementById("autonomo");
+
+        if (estagio.checked == true || clt.checked == true || autonomo.checked == true){
+            alert ("A opção foi selecionada")
+        } else {
+            alert ("Selecione uma opção")
+        }
+    });
+
+    let botaoVT = document.querySelector("#botaoVT");
+    botaoVT.addEventListener("click", function(event){
+    event.preventDefault()
+
+    let ensinomedio = document.getElementById("em");
+    let superiorin = document.getElementById("si");
+    let superiorcu = document.getElementById("scu");
+    let superiorcom = document.getElementById("sc");
+
+    
+    })
